5cc3218df01d64234b34d0dd30426e728c350661
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-03-09 00:31:00 +0500

    push,pop,shift,unshift
    Array method of 
    Push
    Pop
    Shift
    Unshift

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+// let arr: string [] = ["Ahsen", "Sikandar", "Bisma", "Quratulain"]
+Object.defineProperty(exports, "__esModule", { value: true });
+// // console.log(arr)
+// console.log(arr.push("Nida", "Sana", "ahad"))
+// console.log(arr)
+// let arr: string [] = ["Ahsen", "Sikandar", "Bisma", "Quratulain"]
+// // arr.pop()
+// // console.log(arr.push("Nida", "Sana", "ahad"))
+// console.log(arr.pop())
+// let arr: string [] = ["Ahsen", "Sikandar", "Bisma", "Quratulain"]
+// arr.shift()
+// console.log(arr.shift())
+// console.log(arr)
+let arr = ["Ahsen", "Sikandar", "Bisma", "Quratulain"];
+console.log(arr);
+// arr.unshift("Sana")
+console.log(arr.unshift("Sana"));
+console.log(arr);
---
+++ main.ts
@@ -0,0 +1,34 @@
+// let arr: string [] = ["Ahsen", "Sikandar", "Bisma", "Quratulain"]
+
+// // console.log(arr)
+
+// console.log(arr.push("Nida", "Sana", "ahad"))
+
+// console.log(arr)
+
+
+// let arr: string [] = ["Ahsen", "Sikandar", "Bisma", "Quratulain"]
+
+// // arr.pop()
+
+// // console.log(arr.push("Nida", "Sana", "ahad"))
+
+// console.log(arr.pop())
+
+// let arr: string [] = ["Ahsen", "Sikandar", "Bisma", "Quratulain"]
+
+// arr.shift()
+
+// console.log(arr.shift())
+
+// console.log(arr)
+
+
+let arr: string [] = ["Ahsen", "Sikandar", "Bisma", "Quratulain"]
+
+console.log(arr)
+
+// arr.unshift("Sana")
+console.log(arr.unshift("Sana"))
+
+console.log(arr)
